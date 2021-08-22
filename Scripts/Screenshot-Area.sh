3313f2639bbd9e7cc3c3fd58ff0738b45f3a5e09
SpruceLeaf123 <65010788+SpruceLeaf123@users.noreply.github.com>
2021-08-22 13:55:37 +0200

    Update Screenshot-Area.sh

--- Scripts/Screenshot-Area.sh
+++ Scripts/Screenshot-Area.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Allows the user to draw the area on the screen and take its screenshot. The screenshot then gets saved as a PNG file into the folder "Drop" in the Pictures directory in the user's home directory. The file name contains the moment the screenshot was taken.
+# Allows the user to draw the area on the screen and take its screenshot. The screenshot then gets saved as a PNG file into the folder "Drop" in the Pictures directory in the user's home directory (if the directory does not exist, the screenshot might not be saved). The file name contains the moment the screenshot was taken.
 # Requires gnome-screenshot.
 
-moment=$(date)
+moment=$(date +"%Y.%m.%d %H:%M:%S %Z - %A")
 person=$(whoami)
 gnome-screenshot -a -f "/home/$person/Pictures/Drop/$moment.png"
-notify-send -t 1000 "Screenshot probably successful."
+notify-send -t 1000 "Screenshot"
