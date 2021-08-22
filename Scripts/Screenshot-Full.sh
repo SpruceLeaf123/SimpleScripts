1fabd9d8d446c88e6ed9b96b8ec4df25d8519b6e
SpruceLeaf123 <65010788+SpruceLeaf123@users.noreply.github.com>
2021-08-22 13:56:42 +0200

    Update Screenshot-Full.sh

--- Scripts/Screenshot-Full.sh
+++ Scripts/Screenshot-Full.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Takes a screenshot of one monitor (probably the main one) and saves it as PNG file into the folder "Drop" in the Pictures directory in the user's home directory. The file name contains the moment the screenshot was taken.
+# Takes a screenshot of one monitor (probably the main one) and saves it as PNG file into the folder "Drop" in the Pictures directory in the user's home directory (if the directory does not exist, the screenshot might not be saved). The file name contains the moment the screenshot was taken.
 # Requires Scrot.
 
-moment=$(date)
+moment=$(date +"%Y.%m.%d %H:%M:%S %Z - %A")
 person=$(whoami)
 scrot "/home/$person/Pictures/Drop/$moment.png"
-notify-send -t 1000 "Screenshot probably successful."
+notify-send -t 1000 "Screenshot"
