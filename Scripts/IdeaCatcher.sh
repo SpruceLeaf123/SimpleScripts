bf46c5b6db666979aa23b633dc9fd04bb3b82e3b
SpruceLeaf123 <65010788+SpruceLeaf123@users.noreply.github.com>
2021-09-18 21:53:12 +0200

    Update IdeaCatcher.sh

--- Scripts/IdeaCatcher.sh
+++ Scripts/IdeaCatcher.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# If a user has an idea they want to think about later, they can use this script. It shows a simple window which allows a user to write it down. Then the idea gets written to a text file "QuickIdeas" in the home directory of the user (including the time of its creation). Inspired by https://pwlk.pl/produktywnosc-polecane-aplikacje-i-narzedzia/ (section "Skróty w iOS").
+# If a user has an idea they want to think about later, they can use this script. It shows a simple window which allows a user to write it down. Then the idea gets written to a text file "QuickIdeas" in the home directory of the user (including the time of its creation). The user may change the location by editing the variable "location". Inspired by https://pwlk.pl/produktywnosc-polecane-aplikacje-i-narzedzia/ (section "Skróty w iOS").
 # Requires zenity.
+
+location="$HOME"
 
 moment=$(date)
 idea=$(zenity --title "Enter Idea" --entry)
-echo "$moment $idea" >> $HOME/QuickIdeas
+echo "$moment $idea" >> $location/QuickIdeas
