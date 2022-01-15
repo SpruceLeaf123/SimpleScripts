c87e80e76f8392ca48a03f579bf1e126accd8b5d
SpruceLeaf123 <65010788+SpruceLeaf123@users.noreply.github.com>
2022-01-15 12:09:08 +0100

    Update Screenshot-Area.sh

--- Scripts/Screenshot-Area.sh
+++ Scripts/Screenshot-Area.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Allows the user to draw the area on the screen and take its screenshot. The screenshot then gets saved as a PNG file into the folder "Drop" in the Pictures directory in the user's home directory (if the directory does not exist, the screenshot might not be saved). The user may change the directory in which the screenshot is saved by editing the "location" variable. The file name contains the moment the screenshot was taken.
-# Requires gnome-screenshot.
+# Allows the user to draw the area on the screen and take its screenshot. The screenshot then gets saved as a PNG file into the folder "Drop" in the Pictures directory in the user's home directory. The file name contains the moment the screenshot was taken. change the variable "location" to change the place where the screenshots must be saved.
+# Requires maim.
 
-location=$($HOME/Pictures/Drop)
-
-moment=$(date +"%Y.%m.%d %H:%M:%S %Z - %A")
-gnome-screenshot -a -f "$location/$moment.png"
-notify-send -t 1000 "Screenshot"
+location="$HOME/Pictures/Drop/"
+if [[ -d "$location" ]]
+then
+	maim -s "$location/$(date +"%Y.%m.%d %H:%M:%S %Z - %A").png"
+	notify-send -t 1000 "Screenshot probably successful."
+else
+	notify-send -t 1000 "Directory not found."
+fi
